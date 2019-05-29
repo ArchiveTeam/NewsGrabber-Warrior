5bd8c78133e96766e695116582e14c802ba2a393
Matt Iggo <matt@iggo.co.uk>
2019-05-29 16:53:31 +0100

    Update pipeline.py - Added ignore for xx.fbcdn.net

--- pipeline.py
+++ pipeline.py
@@ -248,7 +248,7 @@ class WgetArgs(object):
             '--warc-header', 'operator: Archive Team',
             '--warc-header', 'newsgrabber-dld-script-version: ' + VERSION,
             '--warc-header', ItemInterpolation('ftp-item: %(item_name)s'),
-            '--reject-regex', r'^https?://launcher\.spot\.im/spot/(www\.spot\.im/launcher/|launcher\.spot\.im/|modules/launcher/){3,}bundle\.js$'
+            '--reject-regex', r'(^https?://launcher\.spot\.im/spot/(www\.spot\.im/launcher/|launcher\.spot\.im/|modules/launcher/){3,}bundle\.js)|(https?://static\.xx\.fbcdn\.net/rsrc\.php/)'
         ]
 
         if '-videos' in item_value:
