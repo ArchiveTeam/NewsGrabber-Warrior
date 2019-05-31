a198986faf9781766196cd8375355a3650194bb8
Matt Iggo <matt@iggo.co.uk>
2019-05-31 10:33:06 +0100

    Update warrior-install.sh
    Warrior boot changes

--- warrior-install.sh
+++ warrior-install.sh
@@ -15,14 +15,18 @@ fi
 sudo add-apt-repository ppa:deadsnakes/ppa -y
 sudo apt-get update && sudo apt-get install -y unzip python3.4
 
-sudo pip3 install virtualenv
+if ! sudo pip3 install virtualenv
+then
+  exit 1
+fi
 virtualenv -p /usr/bin/python3.4 pipeline_env
 . pipeline_env/bin/activate
-set +e
-v1.2.3.zip /dev/null 2>&1
-RETVAL=$?
-set -e
-if [ $RETVAL -ne 0 ]; then
+if  [ -f "/usr/local/bin/wpull" ]; 
+  then
+  echo "wpull 1.2.3 already install"
+fi
+if  [ ! -f "v1.2.3.zip" ]; 
+  then 
   echo "Downloading Wpull"
   wget https://github.com/ArchiveTeam/wpull/archive/v1.2.3.zip && unzip -o v1.2.3.zip && cd wpull-1.2.3/
   cp ../wpullsetup.py setup.py
@@ -31,14 +35,15 @@ if [ $RETVAL -ne 0 ]; then
   sudo python3 setup.py install
   cd ..
   echo "Done downloading / installing wpull"
- fi
+fi
 sudo pip3 install lastversion
 #lastversion ytdl-org/youtube-dl
-set +e
-youtube-dl /dev/null 2>&1
-RETVAL=$?
-set -e
-if [ $RETVAL -ne 0 ]; then
+if  [ -f "youtube-dl" ]; 
+  then
+  echo "youtube-dl already exists"
+fi
+if  [ ! -f "youtube-dl" ]; 
+  then
   wget https://github.com/ytdl-org/youtube-dl/releases/download/2019.05.20/youtube-dl
   sudo chmod +x youtube-dl
 fi
