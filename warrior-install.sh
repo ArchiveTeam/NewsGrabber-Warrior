8adc732e881583b28450805fc05f728729a3e765
Matt Iggo <matt@iggo.co.uk>
2019-05-31 13:06:02 +0100

    Update warrior-install.sh
    Rolling back changes as they caused unpredicatable results

--- warrior-install.sh
+++ warrior-install.sh
@@ -1,43 +1,5 @@
 #!/bin/sh -e
 
-echo "installing pip requests"
-if ! sudo pip install requests --upgrade
-then
-  exit 1
-fi
-
-echo "installing pip six"
-if ! sudo pip install six --upgrade
-then
-  exit 1
-fi
-
-sudo add-apt-repository ppa:deadsnakes/ppa -y
-sudo apt-get update && sudo apt-get install -y unzip python3.4
-
-if  [ ! -f "v1.2.3.zip" ]; 
-  then 
-  echo "Downloading Wpull"
-  wget https://github.com/ArchiveTeam/wpull/archive/v1.2.3.zip && unzip -o v1.2.3.zip && cd wpull-1.2.3/
-  cp ../wpullsetup.py setup.py
-  sudo python3 setup.py install 
-  # Yes do this again to fix a bug
-  sudo python3 setup.py install
-  cd ..
-  echo "Done downloading / installing wpull"
-fi
-sudo pip3 install lastversion
-#lastversion ytdl-org/youtube-dl
-if  [ -f "youtube-dl" ]; 
-  then
-  echo "youtube-dl already exists"
-fi
-if  [ ! -f "youtube-dl" ]; 
-  then
-  wget https://github.com/ytdl-org/youtube-dl/releases/download/2019.05.20/youtube-dl
-  sudo cp youtube-dl /usr/local/bin
-  sudo chmod +x /usr/local/bin/youtube-dl
-fi
 
 if ! sudo pip3 freeze | grep -q requests
 then
@@ -57,11 +19,84 @@ then
   fi
 fi
 
+echo "installing pip requests"
+if ! sudo pip install requests --upgrade
+then
+  exit 1
+fi
+
+echo "installing pip six"
+if ! sudo pip install six --upgrade
+then
+  exit 1
+fi
+
 echo "Upgrading pip"
 if ! sudo pip install pip --upgrade
 then
   exit 1
 fi
 
-# check virtual environment for python
-python3 checkvenv.py
+echo "Installing / upgrading youtube_dl"
+if ! sudo pip install youtube_dl --upgrade
+then
+  exit 1
+fi
+
+echo "Checking youtube-dl status"
+if [ -e youtube-dl ]
+then
+  echo "youtube-dl symlink exists"
+else
+  ln -s /usr/local/bin/youtube-dl youtube-dl
+fi
+
+# V wpull V
+
+VERSION='1.2.3'
+TARBALL_32='wpull-1.2.3-linux-i686-3.4.3-20160302011132.zip'
+CHECKSUM_32=0ef742c65336ad4c3205419782f2cec5
+TARBALL_64='wpull-1.2.3-linux-x86_64-3.4.3-20160302011013.zip'
+CHECKSUM_64=e1e792acd2cd0e4b2a5e35d897aa9d64
+INSTALL_DIR="${HOME}/.local/share/wpull-${VERSION}/"
+
+if [ "`uname -m`" = "x86_64" ]; then
+    TARBALL=${TARBALL_64}
+    CHECKSUM=${CHECKSUM_64}
+else
+    TARBALL=${TARBALL_32}
+    CHECKSUM=${CHECKSUM_32}
+fi
+
+DOWNLOAD_URL="https://launchpad.net/wpull/trunk/v${VERSION}/+download/${TARBALL}"
+
+set +e
+${INSTALL_DIR}/wpull --version > /dev/null 2>&1
+RETVAL=$?
+set -e
+
+echo "Test for Wpull returned $RETVAL"
+
+if [ $RETVAL -ne 0 ]; then
+    echo "Downloading Wpull"
+
+    TMP_DIR=/tmp/${CHECKSUM}/
+    mkdir -p ${TMP_DIR}
+
+    wget $DOWNLOAD_URL --continue --directory-prefix ${TMP_DIR}
+
+    echo "Verify checksum"
+    RESULT_CHECKSUM=`md5sum "${TMP_DIR}/${TARBALL}" | cut -f 1 -d " "`
+    if [ "$RESULT_CHECKSUM" != "$CHECKSUM" ]; then
+        echo "Checksum failed. Got ${RESULT_CHECKSUM}. Need ${CHECKSUM}"
+        exit 1
+    fi
+
+    echo "Extracting contents to ${INSTALL_DIR}"
+    mkdir -p "${INSTALL_DIR}"
+    # tar -xzf "${TMP_DIR}/${TARBALL}" --strip-components 1 --directory "${INSTALL_DIR}"
+    python -c "import zipfile; f=zipfile.ZipFile('${TMP_DIR}/${TARBALL}'); f.extractall('${INSTALL_DIR}')"
+    chmod +x ${INSTALL_DIR}/wpull
+
+    echo Done
+fi
