7ebe999ecee805ef2c64005a90e064b2aa7ab93e
Matt Iggo <matt@iggo.co.uk>
2019-06-01 16:42:53 +0100

    Update pipeline.py

--- pipeline.py
+++ pipeline.py
@@ -182,7 +182,7 @@ class DeduplicateWarcExtProc(ExternalProcess):
     def __init__(self):
         ExternalProcess.__init__(self,DeduplicateWarcExtProc)
         
-    def process(self,item):
+    def realize(self,item):
         sourcewarc = "%(item_dir)s/%(warc_file_base)s.warc.gz" % item
         destwarc = "%(item_dir)s/%(warc_file_base)s.deduplicatedwarc.gz" % item
         call(["python", "-u", "dedupe.py", sourcewarc, " ", destwarc])
