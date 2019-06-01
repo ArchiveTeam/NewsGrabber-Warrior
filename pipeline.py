6af278901e3c71125d419d30e2bae55b391842b7
Matt Iggo <matt@iggo.co.uk>
2019-06-01 11:37:03 +0100

    Update pipeline.py

--- pipeline.py
+++ pipeline.py
@@ -55,12 +55,12 @@ WPULL_EXE = find_executable(
     "Wpull",
     re.compile(r"\b1\.2\.3\b"),
     [
+        "/usr/local/bin/wpull",
         "./wpull",
         os.path.expanduser("~/.local/share/wpull-1.2.3/wpull"),
         os.path.expanduser("~/.local/bin/wpull"),
         "/usr/bin/wpull",
         "/usr/local/lib/python3.7/site-packages/wpull",
-        "wpull",
         "./wpull_bootstrap",
 
     ]
